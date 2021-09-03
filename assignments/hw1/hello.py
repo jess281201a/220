9f8bb512c59cb5f8a63be3acd17ed47430a2bfc4
jessicaandrews <andrewsjp@g.cofc.edu>
2021-09-03 10:06:57 -0400

    Completed hw interest

--- assignments/hw1/hello.py
+++ assignments/hw1/hello.py
@@ -1,1 +1,14 @@
-print('Hello World!') 
+"""
+Name: Jessica Andrews
+hello.py
+
+problem:prints hello world
+
+certification of Authenticity
+I certify that this assignment is entirely of my own work
+"""
+def hello():
+
+    print('Hello World!')
+
+hello()
