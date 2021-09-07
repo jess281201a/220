d3f24803ed477652a4f8809b2090c305f4ba3908
jessicaandrews <andrewsjp@g.cofc.edu>
2021-09-07 13:56:44 -0400

    Completed hw interest

--- assignments/hw2/interest.py
+++ assignments/hw2/interest.py
@@ -6,12 +6,13 @@ Problem: A program that computes the monthly interest charge on a credit card ac
 I certify that this assignment is entirely my own work.
 """
 def main():
-
+    #these are input comments for variables
     annual = eval(input('What is your annual interest rate?:'))
     days = eval(input('What is the number of days in your billing cycle?:'))
     previous_balance = eval(input('What is your previous net balance?:'))
     payment_amount = eval(input('What is your payment amount?:'))
     day_of_billing_cycle = eval(input('What is the day of the billing cycle in which the payment was made?:'))
+    #formula's are used here
     monthly_rate = annual/12
     calc1 = previous_balance * days
     days_before_end = days - day_of_billing_cycle
