44dd20ca711bd72c62cb9c43851faa9931862bcc
jessicaandrews <andrewsjp@g.cofc.edu>
2021-09-08 13:14:50 -0400

    Completed interest

--- assignments/hw2/interest.py
+++ assignments/hw2/interest.py
@@ -5,21 +5,27 @@ interest.py
 Problem: A program that computes the monthly interest charge on a credit card account
 I certify that this assignment is entirely my own work.
 """
+
+
 def main():
-    #these are input comments for variables
-    annual = eval(input('What is your annual interest rate?:'))
+
+    # these are input comments for variables
+    rate = eval(input('What is your annual interest rate?:'))
     days = eval(input('What is the number of days in your billing cycle?:'))
     previous_balance = eval(input('What is your previous net balance?:'))
-    payment_amount = eval(input('What is your payment amount?:'))
-    day_of_billing_cycle = eval(input('What is the day of the billing cycle in which the payment was made?:'))
-    #formula's are used here
-    monthly_rate = annual/12
+    payment = eval(input('What is your payment amount?:'))
+    payment_day = eval(input('What is the day of the billing'
+                            ' cycle in which the payment was made?:'))
+    # formula's are used here
+    monthly_rate = rate/12
     calc1 = previous_balance * days
-    days_before_end = days - day_of_billing_cycle
-    calc2 = payment_amount * days_before_end
+    days_before_end = days - payment_day
+    calc2 = payment * days_before_end
     calc3 = calc1 - calc2
     average_daily_balance = calc3 / days
-    monthly = average_daily_balance * monthly_rate/100
-    print('your monthly interest rate is:', monthly)
+    monthly = round(average_daily_balance * monthly_rate/100, 2)
+    print(monthly)
 
-# main()
+
+if __name__ == '__main__':
+    main()
