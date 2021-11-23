8c0dbe0d9fa9c84c91cde6a905fe7157071b9f6d
jessicaandrews <andrewsjp@g.cofc.edu>
2021-11-23 16:33:53 -0500

    completed Lab12

--- labs/lab12/lab12.py
+++ labs/lab12/lab12.py
@@ -58,3 +58,32 @@ def main():
 
 
 main()
+
+
+# algorithm
+"""
+def read_data(filename):
+    i = 0
+    open_file = open(filename,"r")
+    numbers = open_file.read()
+    list = numbers.split()
+    new_list = []
+    while i < len(list):
+        new = eval(list[i])
+        new_list.append(new)
+        i += 1
+    open_file.close()
+    return new_list
+
+def is_in_linear(search_value,values):
+    i = 0
+    try:
+        while i < len(values):
+            find_index = values.index(search_value)
+            if values[find_index] == search_value:
+                answer = True
+            i += 1
+    except:
+        answer = False
+    return answer
+"""
